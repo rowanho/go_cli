dbc8dcfb4f2ebf071a9de98fdd10c02cf71a0070
rowanho <rowanho45@gmail.com>
2020-05-22 15:02:20 +0100

    Pattern matching

--- mv/mv.go
+++ mv/mv.go
@@ -2,10 +2,13 @@ package main
 
 import (
   "github.com/rowanho/go_cli/utilsCli"
+
   "os"
   "flag"
   "fmt"
-  "log"
+  "regexp"
+  "io/ioutil"
+  "path/filepath"
 )
 
 /*
@@ -13,6 +16,10 @@ import (
 */
 
 func directReplace(src string, dst string, prompt bool) (bool, error) {
+  fileInfo, _:= os.Stat(dst)
+  if fileInfo.IsDir() {
+    dst = filepath.Join(dst, filepath.Base(src))
+  }
   _, err := os.Stat(src)
   if os.IsNotExist(err) {
       return false, err
@@ -38,21 +45,70 @@ func directReplace(src string, dst string, prompt bool) (bool, error) {
   return true, nil
 }
 
+
+/*
+* Walks through all matching patterns, moves file if it matches
+*/
+func getFileSet(srcPatterns []string)  (map[string]bool, error) {
+  replaceFiles := make(map[string]bool)
+  err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+			return err
+		}
+    for _, pattern := range srcPatterns {
+      isMatch, matchErr := regexp.MatchString(pattern, path)
+      if matchErr == nil && isMatch {
+        if !info.IsDir() {
+            replaceFiles[path] = true
+        } else {
+            fns,_ := ioutil.ReadDir(path)
+            for _, f := range fns {
+              replaceFiles[f.Name()] = true
+            }
+        }
+      }
+    }
+    return nil
+  });
+  return replaceFiles, err
+}
+
 func main() {
   iPtr := flag.Bool("i", false, "Prompts user when overwriting existing file")
   flag.Parse()
   args := flag.Args()
-  src := args[0]
-  dst := args[1]
+  if len(args) < 2 {
+    fmt.Println("Error: please provide at least 2 arguments, source(s) and destination")
+    return
+  }
+  srcs := args[:len(args) -1]
+  dst := args[len(args) - 1]
 
   prompt := *iPtr
   fmt.Println(prompt)
-  written, err := directReplace(src, dst, prompt)
-  if err != nil {
-    log.Fatal(err)
-  } else if written {
-    fmt.Println(fmt.Sprintf("Moved %s to %s", src, dst))
-  } else {
-    fmt.Println("Cancelled overwrite")
+  fileSet, fileErr := getFileSet(srcs)
+
+  if fileErr != nil {
+    fmt.Println(fileErr.Error())
+    return
+  }
+
+  dstInfo, dstErr := os.Stat(dst)
+  if dstErr != nil {
+    fmt.Println(dstErr.Error())
+    return
+  } else if len(fileSet)> 1 && !dstInfo.IsDir() {
+      fmt.Printf("%s needs to be a directory to move multiple files\n", dst)
+      return
+  }
+  for filePath := range fileSet {
+    written, err := directReplace(filePath, dst, prompt)
+    if err != nil {
+        fmt.Println(err.Error())
+    } else if written {
+        fmt.Printf("Moved %s to %s\n", filePath, dst)
+    } else {
+        fmt.Println("Cancelled overwrite")
+    }
   }
 }
